6b7e6a048d475d116f23d545caaea6b36c2fe834
Robert Lazić <robert.lazicpz@gmail.com>
2023-04-20 22:51:13 +0200

    modified:   src/App.js 	modified:   src/index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const Profile = () => {
+  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+};
+
+export default function Gallery() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <h1>Amazing scientist</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
   );
 }
-
-export default App;

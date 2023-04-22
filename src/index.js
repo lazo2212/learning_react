2f2d5bfd2f58fc3667211c5f8ee8d572350ee3db
Robert Lazić <robert.lazicpz@gmail.com>
2023-04-22 15:15:09 +0200

    deleted:    src/App.js 	new file:   src/Gallery.js 	new file:   src/Profile.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Gallery from './App';
+import Gallery from './Gallery';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

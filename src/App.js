7a0537af95ea81217dc8c5563e595ec705d7d85b
Robert Lazić <robert.lazicpz@gmail.com>
2023-05-04 22:09:31 +0200

    modified:   src/App.css 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,77 +2,58 @@ import React from 'react';
 import { getImageUrl } from './utils.js';
 import './App.css';
 
-const Profile = ({ person, size, profession, awards, discovered }) => {
-  let thumbnailSize = 's';
-  if (size > 90) {
-    thumbnailSize = 'b';
-  }
+const recipes = [
+  {
+    id: 'greek-salad',
+    name: 'Greek Salad',
+    ingredients: ['tomatoes', 'cucumber', 'onion', 'olives', 'feta'],
+  },
+  {
+    id: 'hawaiian-pizza',
+    name: 'Hawaiian Pizza',
+    ingredients: [
+      'pizza crust',
+      'pizza sauce',
+      'mozzarella',
+      'ham',
+      'pineapple',
+    ],
+  },
+  {
+    id: 'hummus',
+    name: 'Hummus',
+    ingredients: ['chickpeas', 'olive oil', 'garlic cloves', 'lemon', 'tahini'],
+  },
+];
 
+const Recepie = ({ id, name, ingredients }) => {
   return (
-    <section className="profile">
-      <h2>{person.name}</h2>
-      <img
-        className="avatar"
-        src={getImageUrl(person.imageId, thumbnailSize)}
-        alt={person.name}
-        width={size}
-        height={size}
-      />
+    <div key={id}>
+      <h2>{name}</h2>
       <ul>
-        <li>
-          <b>Profession: </b>
-          {profession}
-        </li>
-        <li>
-          <b>Awards: {awards.length} </b>({awards.join(', ')})
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {discovered}
-        </li>
+        {ingredients.map((ingredient) => {
+          return <li key={ingredient}>{ingredient}</li>;
+        })}
       </ul>
-    </section>
+    </div>
   );
 };
 
-const Item = ({ name, isPacked }) => {
-  return <li className="item">{isPacked ? name + ' ✔' : name + ' ❌ '}</li>;
+const RecepeList = () => {
+  return (
+    <div>
+      <h1>Recepies</h1>
+      {recipes.map((recepie) => (
+        <Recepie {...recepie} key={recepie.id} />
+      ))}
+    </div>
+  );
 };
 
-export default function Gallery() {
+export default function App() {
   return (
     <>
-      {/* <div>
-        <h1>Notable Scientists</h1>
-        <Profile
-          person={{ name: 'Maria Skłodowska-Curie', imageId: 'szV5sdG' }}
-          size={100}
-          profession="physicist and chemist"
-          awards={[
-            'Nobel Prize in Physics',
-            'Nobel Prize in Chemistry',
-            'Davy Medal',
-            'Matteucci Medal',
-          ]}
-          discovered="polonium (element)"
-        />
-
-        <Profile
-          person={{ name: 'Katsuko Saruhashi', imageId: 'YfeOqp2' }}
-          size={80}
-          profession="geochemist"
-          awards={['Miyake Prize for geochemistry', 'Tanaka Prize']}
-          discovered="a method for measuring carbon dioxide in seawater"
-        />
-      </div> */}
-      <section>
-        <h1>Sally Ride's Packing List</h1>
-        <ul>
-          <Item isPacked={true} name={'Space suit'} />
-          <Item isPacked={true} name={'Helmet with a golden leaf'} />
-          <Item isPacked={false} name={'Photo of Tam'} />
-        </ul>
-      </section>
+      <RecepeList />
     </>
   );
 }

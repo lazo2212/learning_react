0e1fc6cf264ef2f3af3b5bfaa1b2eb29e82c943d
Robert Lazić <robert.lazicpz@gmail.com>
2023-05-11 21:37:24 +0200

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,20 @@ const Gallery = () => {
   const [index, setIndex] = useState(0);
   const [showMore, setShowMore] = useState(false);
 
-  const handleClick = () => {
-    setIndex(index + 1);
+  const handleNextClick = () => {
+    if (index >= sculptureList.length - 1) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  };
+
+  const handlePreviousClick = () => {
+    if (index <= 0) {
+      setIndex(sculptureList.length - 1);
+    } else {
+      setIndex(index - 1);
+    }
   };
 
   const handleMoreClicks = () => {
@@ -18,7 +30,8 @@ const Gallery = () => {
   let sculpture = sculptureList[index];
   return (
     <>
-      <button onClick={handleClick}>Next</button>
+      <button onClick={handleNextClick}>Next</button>
+      <button onClick={handlePreviousClick}>Previous</button>
       <h2>
         <i>{sculpture.name} </i>
         by {sculpture.artist}
